2de6c2e3395fe046810f5f730a09471e67cb80bb
Maria Jose Salinas <mariasalinas713@gmail.com>
2026-09-05 15:50:46 -0600

    - agregando iconos a tab bar - agregando botones para cerrar sesion, ir atras y redirigir a settings en home

--- BookApp/BookApp/src/navigation/TabsNavigator.tsx
+++ BookApp/BookApp/src/navigation/TabsNavigator.tsx
@@ -1,27 +1,48 @@
-import React from "react"
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "../screens/features/Profile";
 import Settings from "../screens/features/Settings";
 import Home from "../screens/Home";
+import { Ionicons } from "@expo/vector-icons";
 
 //1. declarar el tipado de las pantallas con sus parametros
-export type TabsParamList ={
-    Profile: undefined,
-    Settings: undefined, 
-    HomeTab: {email:string},
+export type TabsParamList = {
+  Profile: undefined;
+  Settings: undefined;
+  HomeTab: { email: string };
 };
 
 //2. crear el tabs navigator encargado de manejar la navegacion por pestañas
 const Tab = createBottomTabNavigator<TabsParamList>();
 
 //3. utilizar el navegador por tabs
-export default function TabNavigator (){
-    return(
-        <Tab.Navigator>
-            <Tab.Screen name='Profile' component={Profile} />
-            <Tab.Screen name='Settings' component={Settings}/>
-            <Tab.Screen name='HomeTab' component={Home} initialParams={{email:"usuario"}}/>
-        </Tab.Navigator>
-    )
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile} 
+       options={{
+          title: "Inicio",
+          tabBarIcon: () => (
+            <Ionicons name="person" size={24} color={"#E0E0E0"} />
+          ),
+        }} />
+      <Tab.Screen name="Settings" component={Settings}  options={{
+          title: "Inicio",
+          tabBarIcon: () => (
+            <Ionicons name="settings" size={24} color={"#E0E0E0"} />
+          ),
+        }} />
+      <Tab.Screen
+        name="HomeTab"
+        component={Home}
+        initialParams={{ email: "usuario" }}
+        options={{
+          title: "Inicio",
+          tabBarIcon: () => (
+            <Ionicons name="home" size={24} color={"#E0E0E0"} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
 }
-
--- BookApp/BookApp/src/screens/Home.tsx
+++ BookApp/BookApp/src/screens/Home.tsx
@@ -1,22 +1,65 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { View, Text  } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { RootStackParamList } from "../navigation/StackNavigator";
 import { CompositeScreenProps } from "@react-navigation/native";
 import { BottomTabScreenProps } from "@react-navigation/bottom-tabs";
 import { TabsParamList } from "../navigation/TabsNavigator";
+import CustomButton from "../components/CustomButton";
 
 type NestedProps = CompositeScreenProps<
-    BottomTabScreenProps<TabsParamList, 'HomeTab'>,
-    NativeStackScreenProps<RootStackParamList>
-    >;
+  BottomTabScreenProps<TabsParamList, "HomeTab">,
+  NativeStackScreenProps<RootStackParamList>
+>;
 
-export default function Home({navigation, route}: NestedProps){
-    //extraccion de propiedad de parametros de ruta por medio de destructuring
-    const {email} = route.params;
-    return(
-        <View>
-            <Text>Bienvenido, {email} </Text>
-        </View>
-    )
+export default function Home({ navigation, route }: NestedProps) {
+  //extraccion de propiedad de parametros de ruta por medio de destructuring
+  const { email } = route.params;
+
+  const handleUserSettings = () => {
+    navigation.navigate("Settings");
+  };
+
+  const handleLogout = () => {};
+
+  const handleNavigate = () => {};
+
+  return (
+    <View style={styles.container} >
+      <Text style={styles.welcome}>Bienvenido, {email} </Text>
+
+      <CustomButton
+        title="Ir a Preferencias de Usuario"
+        onPress={handleUserSettings}
+        variant="primary"
+      />
+      <CustomButton
+        title="Cerrar Sesion"
+        variant="secondary"
+        onPress={handleLogout}
+      />
+      <CustomButton
+        title="Ir atras"
+        variant="tertiary"
+        onPress={handleNavigate}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { padding: 24, paddingBottom: 40, alignItems: "center" },
+  welcome: {
+    fontSize: 18,
+    fontWeight: "600",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginTop: 24,
+    marginBottom: 12,
+    alignSelf: "flex-start",
+  },
+});
